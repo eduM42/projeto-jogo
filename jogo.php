7bd30ab736b9725187c6876005b1273166615ad2
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-08-31 14:11:47 -0300

    Update jogo.php

--- jogo.php
+++ jogo.php
@@ -1,3 +1,7 @@
+//ALUNOS: EDUARDO OZORIO (GU302685X), LUCA DE ANDRADE (GU3027813)
+//TURMA A - ADSD2
+//DATA 03 DE SETEMBRO DE 2O22
+
 <?php
     $palavras = array( //Array usado para armazenar as palavras
         "arte",
